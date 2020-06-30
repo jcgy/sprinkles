18189a5f26c220c336dfe74f538a675258f492d3
s0sa <jackgrundy@fastmail.com>
2020-06-30 23:12:46 +0100

    Add fast.ai compatible function

--- README.md
+++ README.md
@@ -4,6 +4,8 @@ Image augmentation function
 Based on [Less Wright's Medium post](https://medium.com/@lessw/progressive-sprinkles-a-new-data-augmentation-for-cnns-and-helps-achieve-new-98-nih-malaria-6056965f671a),
 a function to augment images by adding 'sprinkles' of chosen size and type.
 
+### Stand-alone function
+
 Example use
 ```
 img = cv2.imread("image.png")
@@ -12,4 +14,15 @@ black_sprinkles = sprinkles(img, size=5, perc=0.1)
 frosted_sprinkles = sprinkles(img, size=10, perc=0.1, style='frosted')
 mean_sprinkles = sprinkles(img, size=15, perc=0.1, style='mean')
 ```
-![example image](example.png)
+![stand-alone example image](standalone_example.png)
+
+### fast.ai xtra_tfms() compatible function
+
+Example use
+
+```
+# code snippet from fast.ai dl1 lesson1-pets.ipynb
+data = ImageDataBunch.from_name_re(path_img, fnames, pat,
+ds_tfms=get_transforms(xtra_tfms=[sprinkles(size=10, perc=0.2, style='frosted')]), size=224, bs=bs).normalize(imagenet_stats)
+```
+![fast.ai example image](fastai_example.png)
---
+++ fastai_sprinkles.py
@@ -0,0 +1,85 @@
+import random
+
+def _sprinkles(x, size: int, perc: float, style='black', channels=3):
+    """Produces 'sprinkles' image augmentation on input
+    see: https://medium.com/@lessw/progressive-sprinkles-a-new-data-augmentation-for-cnns-and-helps-achieve-new-98-nih-malaria-6056965f671a
+    
+    Parameters
+    ----------
+    x = pytorch tensor, input image
+    size = int, specifying sprinkle width and height in pixels
+    perc = float, approximate (sprinkles can overlap) percentage of image to occlude
+    style = string, option of ['black', 'frosted', 'mean'] for style of sprinkle
+    channels = int, number of image channels, defaults to 3
+    """
+    x = x.detach().clone()
+    number_of_pixels_to_frost = perc * np.ceil((x.shape[1] * x.shape[2]))
+    number_of_sprinkles = int(np.ceil(number_of_pixels_to_frost / (size * size)))
+    for sprinkle in range(0, number_of_sprinkles):
+        # set boundaries to prevent out of index errors
+        row_options = range((size), (x.shape[1] - size))
+        col_options = range((size), (x.shape[2] - size))
+        # get random index position
+        row = np.random.choice(row_options, replace=False)
+        col = np.random.choice(col_options, replace=False)
+        # change initial pixel value for all channels based on style
+        if style == "black":
+            replacement = 0
+            for c in range(0, channels):
+                x[c, row, col] = replacement
+        elif style == "mean":
+            channel_means = []
+            for c in range(0, channels): 
+                mean = torch.mean(x[c, :, :])
+                channel_means.append(mean)
+            for c in range(0, channels):
+                x[c, row, col] = channel_means[c]
+        elif style == "frosted":
+            for c in range(0, channels):
+                x[c, row, col] = np.random.randint(0, 1)
+        # randomly determine fill direction
+        horizontal_fill_direction = np.random.choice(["left", "right"])
+        vertical_fill_direction = np.random.choice(["up", "down"])
+        # replace pixel values for each sprinkle
+        if (horizontal_fill_direction == "left") & (vertical_fill_direction == "up"):
+            for i in (range(0, (size - 1))):
+                for j in (range(0, (size - 1))):
+                    for c in range(0, channels):
+                        if style == 'frosted':
+                            x[c, (row - j), (col - i)] = np.random.uniform(0, 1)
+                        elif style == 'mean':
+                            x[c, (row - j), (col - i)] = channel_means[c]
+                        else:
+                            x[c, (row - j), (col - i)] = 0
+        elif (horizontal_fill_direction == "left") & (vertical_fill_direction == "down"):
+            for i in (range(0, (size-1))):
+                for j in (range(0, (size-1))):
+                    for c in range(0, channels):
+                        if style == 'frosted':
+                            x[c, (row - j), (col + i)] = np.random.uniform(0, 1)
+                        elif style == 'mean':
+                            x[c, (row - j), (col - i)] = channel_means[c]
+                        else:
+                            x[c, (row - j), (col + i)] = 0
+        elif (horizontal_fill_direction == "right") & (vertical_fill_direction == "up"):
+            for i in (range(0, (size-1))):
+                for j in (range(0, (size-1))):
+                    for c in range(0, channels):
+                        if style == 'frosted':
+                            x[c, (row + j), (col - i)] = np.random.uniform(0, 1)
+                        elif style == 'mean':
+                            x[c, (row - j), (col - i)] = channel_means[c]
+                        else:
+                            x[c, (row + j), (col - i)] = 0
+        elif (horizontal_fill_direction == "right") & (vertical_fill_direction == "down"):
+            for i in (range(0, (size-1))):
+                for j in (range(0, (size-1))):
+                    for c in range(0, channels):
+                        if style == 'frosted':
+                            x[c, (row - j), (col - i)] = np.random.uniform(0, 1)
+                        elif style == 'mean':
+                            x[c, (row - j), (col - i)] = channel_means[c]
+                        else:
+                            x[c, (row - j), (col - i)] = 0
+    return x
+sprinkles = TfmPixel(_sprinkles)
