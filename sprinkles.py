b7059f4433234a486ddd3fe99e6b8d62ed09eb1e
s0sa <jackgrundy@fastmail.com>
2020-06-30 22:50:33 +0100

    Update to handle custom channel number and non-square images

--- sprinkles.py
+++ sprinkles.py
@@ -1,9 +1,8 @@
 import cv2
-import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-def sprinkles(img, size, perc, style='black'):
+def sprinkles(img, size, perc, style='black', channels=3):
     """Produces 'sprinkles' image augmentation on input
     see: https://medium.com/@lessw/progressive-sprinkles-a-new-data-augmentation-for-cnns-and-helps-achieve-new-98-nih-malaria-6056965f671a
     
@@ -13,63 +12,72 @@ def sprinkles(img, size, perc, style='black'):
     size = int specifying sprinkle width and height in pixels
     perc = approximate (sprinkles can overlap) percentage of image to occlude
     style = string, option of ['black', 'frosted', 'mean'] for style of sprinkle
+    channels = int, number of image channels, defaults to 3
     """
     x = img.copy()
-    number_of_pixels_to_frost = perc * np.ceil((x.shape[0] * x.shape[0]))
-    number_of_sprinkles = int(np.ceil(number_of_pixels_to_frost / (size * size)))
-    # TODO need to handle RGB channels - multiple arrays
+    number_of_pixels_to_change = perc * np.ceil((x.shape[1] * x.shape[2]))
+    number_of_sprinkles = int(np.ceil(number_of_pixels_to_change / (size * size)))
     for sprinkle in range(0, number_of_sprinkles):
-        # set boundaries to preven out of index errors
-        options = range((size), (x.shape[0] - size))
+        # set boundaries to prevent out of index errors
+        row_options = range((size), (x.shape[1] - size))
+        col_options = range((size), (x.shape[2] - size))
         # get random index position
-        row = np.random.choice(options, replace=False)
-        col = np.random.choice(options, replace=False)
-        # change initial pixel value
-        x[row, col] = np.random.randint(0, 255)
+        row = np.random.choice(row_options, replace=False)
+        col = np.random.choice(col_options, replace=False)
+        # change initial pixel value for all channels based on style
+        if style == "black":
+            replacement = 0
+            for c in range(0, channels):
+                x[c, row, col] = replacement
+        elif style == "mean":
+            channel_means = cv2.mean(x)
+            for c in range(0, channels):
+                x[c, row, col] = channel_means[c]
+        elif style == "frosted":
+            for c in range(0, channels):
+                x[c, row, col] = np.random.randint(0, 1)
         # randomly determine fill direction
         horizontal_fill_direction = np.random.choice(["left", "right"])
         vertical_fill_direction = np.random.choice(["up", "down"])
-        if style == 'mean':
-            mean = cv2.mean(x)
         # replace pixel values
         if (horizontal_fill_direction == "left") & (vertical_fill_direction == "up"):
             for i in (range(0, (size - 1))):
                 for j in (range(0, (size - 1))):
-                    for c in [0, 1, 2]:
+                    for c in range(0, channels):
                         if style == 'frosted':
                             x[(row - j), (col - i)][c] = np.random.randint(0, 255)
                         elif style == 'mean':
-                            x[(row - j), (col - i)][c] = mean[c]
+                            x[(row - j), (col - i)][c] = channel_means[c]
                         else:
                             x[(row - j), (col - i)] = 0
         elif (horizontal_fill_direction == "left") & (vertical_fill_direction == "down"):
             for i in (range(0, (size-1))):
                 for j in (range(0, (size-1))):
-                    for c in [0, 1, 2]:
+                    for c in range(0, channels):
                         if style == 'frosted':
                             x[(row - j), (col + i)][c] = np.random.randint(0, 255)
                         elif style == 'mean':
-                            x[(row - j), (col - i)][c] = mean[c]
+                            x[(row - j), (col - i)][c] = channel_means[c]
                         else:
                             x[(row - j), (col + i)] = 0
         elif (horizontal_fill_direction == "right") & (vertical_fill_direction == "up"):
             for i in (range(0, (size-1))):
                 for j in (range(0, (size-1))):
-                    for c in [0, 1, 2]:
+                    for c in range(0, channels):
                         if style == 'frosted':
                             x[(row + j), (col - i)][c] = np.random.randint(0, 255)
                         elif style == 'mean':
-                            x[(row - j), (col - i)][c] = mean[c]
+                            x[(row - j), (col - i)][c] = channel_means[c]
                         else:
                             x[(row + j), (col - i)] = 0
         elif (horizontal_fill_direction == "right") & (vertical_fill_direction == "down"):
             for i in (range(0, (size-1))):
                 for j in (range(0, (size-1))):
-                    for c in [0, 1, 2]:
+                    for c in range(0, channels):
                         if style == 'frosted':
                             x[(row - j), (col - i)][c] = np.random.randint(0, 255)
                         elif style == 'mean':
-                            x[(row - j), (col - i)][c] = mean[c]
+                            x[(row - j), (col - i)][c] = channel_means[c]
                         else:
                             x[(row - j), (col - i)] = 0
     return np.array(x)
