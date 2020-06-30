ee41adac3a44bebf8c58eff6372182c0b23a3e9c
s0sa <jackgrundy@fastmail.com>
2020-06-30 23:33:13 +0100

    Rename internal image reference x to img

--- fastai_sprinkles.py
+++ fastai_sprinkles.py
@@ -12,13 +12,13 @@ def _sprinkles(x, size: int, perc: float, style='black', channels=3):
     style = string, option of ['black', 'frosted', 'mean'] for style of sprinkle
     channels = int, number of image channels, defaults to 3
     """
-    x = x.detach().clone()
-    number_of_pixels_to_frost = perc * np.ceil((x.shape[1] * x.shape[2]))
+    img = x.detach().clone()
+    number_of_pixels_to_frost = perc * np.ceil((img.shape[1] * img.shape[2]))
     number_of_sprinkles = int(np.ceil(number_of_pixels_to_frost / (size * size)))
     for sprinkle in range(0, number_of_sprinkles):
         # set boundaries to prevent out of index errors
-        row_options = range((size), (x.shape[1] - size))
-        col_options = range((size), (x.shape[2] - size))
+        row_options = range((size), (img.shape[1] - size))
+        col_options = range((size), (img.shape[2] - size))
         # get random index position
         row = np.random.choice(row_options, replace=False)
         col = np.random.choice(col_options, replace=False)
@@ -26,17 +26,17 @@ def _sprinkles(x, size: int, perc: float, style='black', channels=3):
         if style == "black":
             replacement = 0
             for c in range(0, channels):
-                x[c, row, col] = replacement
+                img[c, row, col] = replacement
         elif style == "mean":
             channel_means = []
             for c in range(0, channels): 
-                mean = torch.mean(x[c, :, :])
+                mean = torch.mean(img[c, :, :])
                 channel_means.append(mean)
             for c in range(0, channels):
-                x[c, row, col] = channel_means[c]
+                img[c, row, col] = channel_means[c]
         elif style == "frosted":
             for c in range(0, channels):
-                x[c, row, col] = np.random.randint(0, 1)
+                img[c, row, col] = np.random.randint(0, 1)
         # randomly determine fill direction
         horizontal_fill_direction = np.random.choice(["left", "right"])
         vertical_fill_direction = np.random.choice(["up", "down"])
@@ -46,40 +46,40 @@ def _sprinkles(x, size: int, perc: float, style='black', channels=3):
                 for j in (range(0, (size - 1))):
                     for c in range(0, channels):
                         if style == 'frosted':
-                            x[c, (row - j), (col - i)] = np.random.uniform(0, 1)
+                            img[c, (row - j), (col - i)] = np.random.uniform(0, 1)
                         elif style == 'mean':
-                            x[c, (row - j), (col - i)] = channel_means[c]
+                            img[c, (row - j), (col - i)] = channel_means[c]
                         else:
-                            x[c, (row - j), (col - i)] = 0
+                            img[c, (row - j), (col - i)] = 0
         elif (horizontal_fill_direction == "left") & (vertical_fill_direction == "down"):
             for i in (range(0, (size-1))):
                 for j in (range(0, (size-1))):
                     for c in range(0, channels):
                         if style == 'frosted':
-                            x[c, (row - j), (col + i)] = np.random.uniform(0, 1)
+                            img[c, (row - j), (col + i)] = np.random.uniform(0, 1)
                         elif style == 'mean':
-                            x[c, (row - j), (col - i)] = channel_means[c]
+                            img[c, (row - j), (col - i)] = channel_means[c]
                         else:
-                            x[c, (row - j), (col + i)] = 0
+                            img[c, (row - j), (col + i)] = 0
         elif (horizontal_fill_direction == "right") & (vertical_fill_direction == "up"):
             for i in (range(0, (size-1))):
                 for j in (range(0, (size-1))):
                     for c in range(0, channels):
                         if style == 'frosted':
-                            x[c, (row + j), (col - i)] = np.random.uniform(0, 1)
+                            img[c, (row + j), (col - i)] = np.random.uniform(0, 1)
                         elif style == 'mean':
-                            x[c, (row - j), (col - i)] = channel_means[c]
+                            img[c, (row - j), (col - i)] = channel_means[c]
                         else:
-                            x[c, (row + j), (col - i)] = 0
+                            img[c, (row + j), (col - i)] = 0
         elif (horizontal_fill_direction == "right") & (vertical_fill_direction == "down"):
             for i in (range(0, (size-1))):
                 for j in (range(0, (size-1))):
                     for c in range(0, channels):
                         if style == 'frosted':
-                            x[c, (row - j), (col - i)] = np.random.uniform(0, 1)
+                            img[c, (row - j), (col - i)] = np.random.uniform(0, 1)
                         elif style == 'mean':
-                            x[c, (row - j), (col - i)] = channel_means[c]
+                            img[c, (row - j), (col - i)] = channel_means[c]
                         else:
-                            x[c, (row - j), (col - i)] = 0
-    return x
+                            img[c, (row - j), (col - i)] = 0
+    return img
 sprinkles = TfmPixel(_sprinkles)
